807fd273e1e088d0e7025e2808082ba848054a0a
nishanshashintha1996 <shashinthanishan00@gmail.com>
2024-04-29 21:10:05 +0200

    fresh commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Hero from './components/hero/Hero';
+
+export default function App() {
+  return (
+    <>
+      <Header></Header>
+      <Hero></Hero>
+      <Footer></Footer>
+    </>
+  )
+}
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,84 @@
+import { useState, useEffect } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Papa from 'papaparse';
+import Data from './../../db/mirn-exact.csv';
+
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+]
+
+export default function Hero() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(Data);
+      const reader = response.body.getReader();
+      const result = await reader.read();
+      const decoder = new TextDecoder('utf-8');
+      const csvData = decoder.decode(result.value);
+      const parsedData = Papa.parse(csvData, {
+        header:true,
+        skipEmptyLines:true
+      }).data;
+      setData(parsedData);
+    };
+    fetchData();
+  }, [])
+
+  return (
+    <div className="bg-white">
+
+      <div className="relative isolate px-6 pt-14 lg:px-8">
+        <div className="mx-auto max-w-2xl py-20 sm:py-15 lg:py-20">
+          <div className="text-center">
+
+            {
+              data.map((row, index) => {
+                console.log(row);
+              })
+            }
+
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Data to enrich your online business
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+              fugiat veniam occaecat fugiat aliqua.
+            </p>
+            <div className="mt-10 flex items-center justify-center gap-x-6">
+              
+            </div>
+            <div className="sm:col-span-4">
+              <div className="mt-2">
+                <input
+                  id="search"
+                  name="text"
+                  type="text"
+                  autoComplete="text"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
