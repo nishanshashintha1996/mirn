785418dc4cb9d690e0480f629b92b2fb5b3c68c0
nishanshashintha1996 <shashinthanishan00@gmail.com>
2024-05-19 23:25:39 +0200

    house number update

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -25,6 +25,7 @@ export default function Hero() {
   const [city, setCity] = useState([]);
   const [state, setState] = useState([]);
   const [postal, setPostal] = useState([]);
+  const [houseNumber, setHouseNumber] = useState([]);
 
   useEffect(() => {
       //console.log(data);
@@ -39,7 +40,8 @@ export default function Hero() {
         streetAddress:streetAddress,
         city:city,
         state:state,
-        postal:postal 
+        postal:postal,
+        houseNumber:houseNumber
       })
     };
 
@@ -81,6 +83,23 @@ export default function Hero() {
                     value={streetAddress}
                     onChange = {e => setStreetAddress(e.target.value)}
                     autoComplete="street-address"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label htmlFor="house-number" className="block text-sm font-medium leading-6 text-gray-900">
+                  House Number
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    name="house-number"
+                    id="house-number"
+                    value={houseNumber}
+                    onChange = {e => setHouseNumber(e.target.value)}
+                    autoComplete="house-number"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
